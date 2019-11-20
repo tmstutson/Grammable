cab387d6cc85f3d6829d6887f08e60eb2a16e105
Tia Stutson <tmstutson@gmail.com>
2019-11-20 01:43:39 +0000

    edit and update action test for grams

--- app/controllers/grams_controller.rb
+++ app/controllers/grams_controller.rb
@@ -1,13 +1,21 @@
 class GramsController < ApplicationController
 	before_action :authenticate_user!, only: [:new, :create]
 
-	
+
 	def new
 		@gram = Gram.new
 	end 
 
 	def index
 	end 
+
+
+	def show
+    	@gram = Gram.find_by_id(params[:id])
+  		if @gram.blank?
+    		render plain: 'Not Found :(', status: :not_found
+		end
+	end
 
 	def create 
 		@gram = current_user.grams.create(gram_params)
--- spec/controllers/grams_controller_spec.rb
+++ spec/controllers/grams_controller_spec.rb
@@ -1,11 +1,37 @@
 require 'rails_helper'
 
 RSpec.describe GramsController, type: :controller do
-  describe "grams#index action" do
-    it "should successfully show the page" do
-      get :index
+	describe "grams#edit action" do
+		it "should successfully show the edit form if the gram is found" do 
+			gram = FactoryBot.edit(:gram)
+			get :edit, params: { id: gram.id }
+			expect(response).to have_http_status(:success)
+	end 
+
+		it "should return a 404 error message if the gram is not found" do 
+			get :edit, params: { id: 'SWAG' }
+			expect(response).to have_http_status(:success)
+		end 
+	end 
+
+
+
+
+
+
+
+
+  describe "grams#show action" do
+    it "should successfully show the page if the gram is found" do
+      gram = FactoryBot.create(:gram)
+      get :show, params: { id: gram.id }
       expect(response).to have_http_status(:success)
-    end
+      end 
+
+    it "should return a 404 error if the gram is not found" do
+  		get :show, params: { id: 'TACOCAT' }
+  		expect(response).to have_http_status(:not_found)
+	end
   end
 
 
